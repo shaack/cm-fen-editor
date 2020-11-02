dce9db8bc4c38039bb91e08cc0b59f9c92db1061
Stefan Haack <shaack@gmail.com>
2020-11-02 23:26:28 +0100

    chessboard is viewed

--- src/cm-fen-editor/FenEditor.js
+++ src/cm-fen-editor/FenEditor.js
@@ -3,7 +3,24 @@
  * Repository: https://github.com/shaack/cm-fen-editor
  * License: MIT, see file 'LICENSE'
  */
+import {Chessboard} from "../../lib/cm-chessboard/Chessboard.js"
 
-class FenEditor {
+export class FenEditor {
+    constructor(element, props = {}) {
+        this.element = element
+        this.props = {
 
+        }
+        Object.assign(this.props, props)
+        this.elements = {
+            fenInputOutput: this.element.querySelector("#fenInputOutput"),
+            chessboard: this.element.querySelector(".chessboard")
+        }
+        this.chessboard = new Chessboard(this.elements.chessboard, {
+            position: "start",
+            sprite: {
+                url: "./node_modules/cm-chessboard/assets/images/chessboard-sprite.svg", // pieces and markers are stored es svg in the sprite
+            }
+        })
+    }
 }
