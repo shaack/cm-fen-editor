aab88767d6d929b694a1cb2e392d6300333b65ce
Stefan Haack <shaack@gmail.com>
2020-11-17 21:14:16 +0100

    Anzeige Buttons Mode und FEN

--- src/cm-fen-editor/FenEditor.js
+++ src/cm-fen-editor/FenEditor.js
@@ -10,17 +10,34 @@ export const EDIT_MODE = {
 export class FenEditor extends Component {
     constructor(context, props) {
         super({}, {
-            editMode: EDIT_MODE.move
+            editMode: EDIT_MODE.move,
+            fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         }, {
             editMode: {
                 callback: (value) => {
-                    console.log("editMode callback", value)
+                    setTimeout(() => {
+                        for (const button of this.elements.modeButtons) {
+                            if (this.state.editMode === button.dataset.mode) {
+                                button.classList.add("active")
+                            } else {
+                                button.classList.remove("active")
+                            }
+                        }
+                    })
+
                 }
-            }
+            },
+            fen: "#fenInputOutput"
         }, {
             switchMode: (event) => {
                 this.state.editMode = event.target.dataset.mode
             }
         }, context)
+        this.elements = {
+            modeButtons: context.querySelectorAll("button[data-mode]"),
+        }
+    }
+    test() {
+
     }
 }
